db5b23da6b4cf9a75a6bf7eff7c1462dd94c349c
alessandrocab <alessandrocabriolu@hotmail.com>
2025-05-20 11:50:00 +0200

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -14,7 +14,7 @@ function draw() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "#0F0";
+  ctx.fillStyle = "rgba(0, 255, 0, 0.07)";
   ctx.font = fontSize + "px monospace";
 
   for (let i = 0; i < drops.length; i++) {
